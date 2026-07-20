c1b7b2aa895a5db834e10754aee60fdce2c6eedc
atulchakwa <atulchakwa41@gmail.com>
2026-07-20 15:16:52 +0530

    Fix: UserModel has no stationName

--- app/lib/view/common_workers/worker_mobile_home_screen.dart
+++ app/lib/view/common_workers/worker_mobile_home_screen.dart
@@ -845,18 +845,17 @@ class WorkerMobileHomeScreen extends StatelessWidget {
                     onTap: () {
                       final title = action['title'] as String;
                       final sid = controller.currentUser.value?.stationId;
-                      final sname = controller.currentUser.value?.stationName;
                       Widget page;
                       if (title == 'Complaints') {
                         page = const WorkerComplaintsScreen();
                       } else if (title == 'Station Cleaning') {
                         page = const WorkerStationCleaningScreen();
                       } else if (title == 'Pest Control') {
-                        page = WorkerPestControlScreen(stationId: sid, stationName: sname);
+                        page = WorkerPestControlScreen(stationId: sid);
                       } else if (title == 'Machines') {
                         page = const WorkerMachineScreen();
                       } else if (title == 'Garbage') {
-                        page = WorkerGarbageScreen(stationId: sid, stationName: sname);
+                        page = WorkerGarbageScreen(stationId: sid);
                       } else if (title == 'Audit Trail') {
                         page = const WorkerAuditScreen();
                       } else {
